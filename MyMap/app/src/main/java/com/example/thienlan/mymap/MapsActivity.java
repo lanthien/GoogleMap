325302ca24d6c226d58196f2eaa614868989d03f
lanthien <lan.thiendiep@gmail.com>
2016-01-19 01:32:23 +0700

    Revert "Update project"
    This reverts commit 55123b42c98458b470cf288cc5293ff449de699c.

--- MyMap/app/src/main/java/com/example/thienlan/mymap/MapsActivity.java
+++ MyMap/app/src/main/java/com/example/thienlan/mymap/MapsActivity.java
@@ -1,24 +1,16 @@
 package com.example.thienlan.mymap;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 
 import com.example.thienlan.mymap.Code.GPSTracker;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,18 +18,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     protected double longtitude, latitude;
     GPSTracker gpsTracker = null;
-    LocationManager locationManager;
-    String address, province, ward, city, country;
-    PlaceAutocompleteFragment autocompleteFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,34 +32,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager()
-                .findFragmentById(R.id.place_autocomplete_fragment);
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                mMap.clear();
-                latitude = place.getLatLng().latitude;
-                longtitude = place.getLatLng().longitude;
-                AddMarker(place.getName().toString());
-            }
-
-            @Override
-            public void onError(Status status) {
-                Log.d("Place: ", status.getStatus().toString());
-            }
-        });
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    3000,//update per 3s
-                    1,//update per 1m
-                    new MapsActivity()
-            );
-        }
-
     }
 
     /**
@@ -95,14 +52,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             /*Set Location button enable*/
             mMap.setMyLocationEnabled(true);
         }
-
         mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
                 if (gpsTracker.canGetLocation()) {
                     latitude = gpsTracker.getLatitude();
                     longtitude = gpsTracker.getLongitude();
-                    AddMarker("You are here");
+                    /*Add a marker in Sydney and move the camera*/
+                    LatLng sydney = new LatLng(latitude, longtitude);
+                    mMap.addMarker(new MarkerOptions().position(sydney)
+                            .title("You are here")
+                            .snippet(latitude + ", " + longtitude));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
                 } else {
                     // can't get location
                     // GPS or Network is not enabled
@@ -112,55 +73,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return false;
             }
         });
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        latitude = location.getLatitude();
-        longtitude = location.getLongitude();
-        AddMarker("You are here");
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    /* Add Marker on map
-     * place: add marker's title */
-    public void AddMarker(String place) {
-        Geocoder geocoder;
-        List<Address> addresses;
-        geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
-
-        try {
-            addresses = geocoder.getFromLocation(latitude, longtitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-
-            //Check item null
-            address = addresses.get(0).getAddressLine(0) != null ? addresses.get(0).getAddressLine(0) : "";
-            city = addresses.get(0).getLocality() != null ? addresses.get(0).getLocality() : "";
-            province = addresses.get(0).getSubLocality() != null ? addresses.get(0).getSubLocality() : "";
-            ward = addresses.get(0).getSubAdminArea() != null ? addresses.get(0).getSubAdminArea() : "";
-            country = addresses.get(0).getCountryName() != null ? addresses.get(0).getCountryName() : "";
-
-            /*Add a marker in Sydney and move the camera*/
-            LatLng sydney = new LatLng(latitude, longtitude);
-            mMap.addMarker(new MarkerOptions().position(sydney)
-                    .title(place).snippet(address + " " + province + " " + ward + " " + city + " " + country + "\nLatLng: " + latitude + " " + longtitude)
-                    .draggable(true));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
