189c98b54c30299dd59c17a41b78c8011b0c3706
lanthien <lan.thiendiep@gmail.com>
2016-01-25 00:58:15 +0700

    Find Patch (doing)
    Parse json

--- MyMap/app/src/main/java/com/example/thienlan/mymap/DrawDirection.java
+++ MyMap/app/src/main/java/com/example/thienlan/mymap/DrawDirection.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.thienlan.mymap.Code.MyTask;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.koushikdutta.async.future.Future;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
@@ -17,6 +20,7 @@ import com.koushikdutta.ion.future.ResponseFuture;
 import org.json.JSONObject;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 /**
  * Created by ThienLan on 22/01/2016.
@@ -28,6 +32,9 @@ public class DrawDirection {
     JsonArray jsonArray;
     GoogleMap googleMap;
     LatLng src, dest;
+    Gson gson;
+    JsonParser jsonParser;
+    ArrayList jsonObjList;
 
     public DrawDirection(Context context,GoogleMap googleMap, LatLng src, LatLng dest)
     {
@@ -45,7 +52,9 @@ public class DrawDirection {
             + "&sensor=false&units=metric&mode=driving";
 
         node = new JsonObject();
-
+        gson = new Gson();
+        jsonParser=new JsonParser();
+        jsonArray = new JsonArray();
 
         Ion.with(context)
                 .load(url)
@@ -53,20 +62,26 @@ public class DrawDirection {
                 .setCallback(new FutureCallback<JsonObject>() {
                     @Override
                     public void onCompleted(Exception e, JsonObject result) {
-                        node = result;
-                        jsonArray=node.getAsJsonArray("geocoded_waypoints");
+                        try {
+//                            jsonObjList = gson.fromJson(result.getAsJsonArray("routes"),ArrayList.class);
+//                            System.out.println("List Elements are  : "+result.get("steps").toString());
+//                            System.out.println("List Elements are  : "+jsonObjList.toString());
+                            jsonArray = result.getAsJsonArray("routes");
+                            jsonObjList = gson.fromJson(jsonArray, ArrayList.class);
+                            System.out.println("List Elements are  : "+jsonObjList.size());
+                            String[] x = jsonObjList.get(0).toString().split(",");
+                            for(String l : x){
+                                System.out.println("my data: "+ l);
+                            }
+                            System.out.println("data: "+  jsonObjList.get(0).toString().split(","));
+                        }
+                        catch (Exception ex)
+                        {
+                            System.out.println("Error: "+ex.getMessage());
+                        }
                     }
                 });
         //jsonArray=node.getAsJsonArray("abridged_cast");
 
-    }
-
-    private class Mytask extends AsyncTask<Void,Void,Void>
-    {
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            return null;
-        }
     }
 }
